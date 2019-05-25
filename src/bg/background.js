1650207590774007a0177ce11f00f9c628727710
Dobmeier <jakob.dobmeier@st.oth-regensburg.de>
2019-05-25 02:26:28 +0200

    Parser for Baddomains

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,4 +1,5 @@
 var thirdParty = ['test'];
+var blacklist = [];
 
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
     chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function(tabs) {
@@ -7,8 +8,8 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
         let baseUrl = tabs[0].url;
         let url = details.url;
 
-        console.log("requestURL --> " + details.url);
-        console.log("siteUrl: -> " + baseUrl);
+        //console.log("requestURL --> " + details.url);
+        //console.log("siteUrl: -> " + baseUrl);
 
 
 
@@ -22,14 +23,30 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
 }, { urls: ["<all_urls>"] }, ["blocking"]);
 
 
-(function setup() {
+(function setup(){
+    var list = "";
+    var split = [];
     $.ajax({
-        url: "../../easylist.txt",
-        success: function(data) {
-
+        url: "http://winhelp2002.mvps.org/hosts.txt",
+        success: function(data){
+            list = data;
+            //console.log(data);
         }
-    });
-}());
+    })
+    var c = 0;
+    split = list.split(" ");
+    console.log(split);
+    for(var i = 0; i < split.length; i++){
+        if(split[i].charAt(0) == '0'){
+            console.log(split.split("0.0.0.0 ")[0]);
+            blacklist[c++] = split.split("0.0.0.0 ")[0];
+        }
+    }
+    for(var i = 0; i < 50; i++){
+        console.log(blacklist[i]);
+    }
+
+})();
 
 //loads the app filter list data 
 chrome.runtime.onInstalled.addListener(function() {
